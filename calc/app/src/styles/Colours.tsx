12055c8b6998e38334aa2390154c15af50a5f944
danielrguezh <danielrh111005@gmail.com>
2025-10-07 16:23:02 +0100

    algo

---
+++ calc/app/src/styles/Colours.tsx
@@ -0,0 +1,11 @@
+const myColours= {
+    light: '#F1F2F3',
+    dark: '#17171c',
+    blue: '#4B5EFC',
+    bntGray: '4E505F',
+    bntDark: '#2E2F38',
+    gray: '#747477',
+    black: '#000000',
+    white: '#FFFFFF',
+    result: '#46D5B2',
+}
